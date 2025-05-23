7025efea7c0d1ec6556ee7bdf102e8f6f7a56864
sinchuktaras <grivus062006@gmail.com>
2025-05-23 12:57:19 +0300

    Add unit tests for model and views

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Image
+from django.utils import timezone
+from datetime import timedelta
+
+def gallery_view(request):
+    month_ago = timezone.now() - timedelta(days=30)
+    images = Image.objects.filter(created_at__gte=month_ago)
+    return render(request, 'gallery.html', {'images': images})
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+    return render(request, 'image_detail.html', {'image': image})
--- project_gallery/urls.py
+++ project_gallery/urls.py
@@ -17,14 +17,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from gallery import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.gallery_view, name='main'),
     path('image/<int:pk>/', views.image_detail, name='image_detail'),
+    path('gallery/', include('gallery.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
